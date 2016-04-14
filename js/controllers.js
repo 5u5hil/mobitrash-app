cef7d63a9ff8ab9f445942fb4ed3f79bd4d0a50a
sushilcs111 <sushil.sud@gmail.com>
2016-04-15 02:10:51 +0530

    initial

---
+++ js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('markAttendanceCtrl', function($scope) {
+
+})
+   
+.controller('scheduleForTheDayCtrl', function($scope) {
+
+})
+   
+.controller('cleaningCtrl', function($scope) {
+
+})
+      
+.controller('receiptsCtrl', function($scope) {
+
+})
+   
+.controller('getClickedCtrl', function($scope) {
+
+})
+   
+.controller('pickupDetailsCtrl', function($scope) {
+
+})
+ 
---
+++ js/routes.js
@@ -0,0 +1,75 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('markAttendance2.markAttendance', {
+    url: '/attendance',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/markAttendance.html',
+        controller: 'markAttendanceCtrl'
+      }
+    }
+  })
+
+  .state('markAttendance2.scheduleForTheDay', {
+    url: '/schedule',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/scheduleForTheDay.html',
+        controller: 'scheduleForTheDayCtrl'
+      }
+    }
+  })
+
+  .state('markAttendance2.cleaning', {
+    url: '/cleaning',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/cleaning.html',
+        controller: 'cleaningCtrl'
+      }
+    }
+  })
+
+  .state('markAttendance2', {
+    url: '/attendance',
+    templateUrl: 'templates/markAttendance2.html',
+    abstract:true
+  })
+
+  .state('markAttendance2.receipts', {
+    url: '/receipts',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/receipts.html',
+        controller: 'receiptsCtrl'
+      }
+    }
+  })
+
+  .state('getClicked', {
+    url: '/click',
+    templateUrl: 'templates/getClicked.html',
+    controller: 'getClickedCtrl'
+  })
+
+  .state('pickupDetails', {
+    url: '/details',
+    templateUrl: 'templates/pickupDetails.html',
+    controller: 'pickupDetailsCtrl'
+  })
+
+$urlRouterProvider.otherwise('/attendance/attendance')
+
+  
+
+});
