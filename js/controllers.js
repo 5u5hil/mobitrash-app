f9f8506514a0eb81f97d4c55188961f5dcf24f54
sushilcs111 <sushil.sud@gmail.com>
2016-04-15 15:30:40 +0530

    changes

--- js/controllers.js
+++ js/controllers.js
@@ -1,26 +1,26 @@
 angular.module('app.controllers', [])
-  
-.controller('markAttendanceCtrl', function($scope) {
 
-})
-   
-.controller('scheduleForTheDayCtrl', function($scope) {
+        .controller('markAttendanceCtrl', function ($scope) {
 
-})
-   
-.controller('cleaningCtrl', function($scope) {
+        })
 
-})
-      
-.controller('receiptsCtrl', function($scope) {
+        .controller('scheduleForTheDayCtrl', function ($scope) {
 
-})
-   
-.controller('getClickedCtrl', function($scope) {
+        })
 
-})
-   
-.controller('pickupDetailsCtrl', function($scope) {
+        .controller('cleaningCtrl', function ($scope) {
 
-})
+        })
+
+        .controller('receiptsCtrl', function ($scope) {
+
+        })
+
+        .controller('getClickedCtrl', function ($scope) {
+
+        })
+
+        .controller('pickupDetailsCtrl', function ($scope) {
+
+        })
  
